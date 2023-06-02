0c5b99df20173a6532fa02d9f68af528e8c2cea5
zhangyongfa <zhangyongfa@xindecolink.com>
2023-06-02 18:08:43 +0800

    init

---
+++ src/pages/RfidChip/RfidChip.vue
@@ -0,0 +1,17 @@
+<template>
+  <canvas id="webgl">浏览器不支持canvas，请切换浏览器重试</canvas>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+#webgl{
+  position: fixed;
+  top: 0;
+  left: 0;
+  outline: none;
+}
+
+</style>
---
+++ src/pages/RfidChip/index.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+export const initRFID = () => {
+  // 获取canvas元素
+  const canvas = document.getElementById('webgl');
+  
+  // 创建场景
+  const scene = new THREE.Scene();
+
+  // 创建相机
+      
+}
